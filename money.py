7af051a111ed0e45f749bb4d92d88c672c1b0027
Genola Johnson <genolajohnson@gmail.com>
2020-06-28 18:08:18 +0000

    Line 31 has an error I can't locate

--- money.py
+++ money.py
@@ -2,63 +2,93 @@
 
 
 class DifferentCurrencyError(Exception):
-    pass
+    
 
 
-class Currency:
-    """
-    Represents a currency. Does not contain any exchange rate info.
-    """
+ class Currency:
+    # """
+    # Represents a currency. Does not contain any exchange rate info.
+    # """
 
     def __init__(self, name, code, symbol=None, digits=2):
-        """
-        Parameters:
-        - name -- the English name of the currency
-        - code -- the ISO 4217 three-letter code for the currency
-        - symbol - optional symbol used to designate currency
-        - digits -- number of significant digits used
-        """
-        pass
+
+        self.name = name
+        self.code = code
+        self.symbol = symbol
+        self.digits = digits
+
+        # """ class is currency self.name, code, symbol, digits are instances
+        # Parameters:
+        # - name -- the English name of the currency
+        # - code -- the ISO 4217 three-letter code for the currency USD
+        # - symbol - optional symbol used to designate currency
+        # - digits -- number of significant digits used 2
+        # """
+        
 
     def __str__(self):
-        """
-        Should return the currency code, or code with symbol in parentheses.
-        """
-        pass
+        if self.symbol:
+            return f"{self.code}({self.symbol})
+        else:
+            return f"{self.code}
 
+        # """  
+        # Should return the currency code, or code with symbol in parentheses.
+        # """
+        # pass
+
+class Name:
     def __eq__(self, other):
-        """
-        All fields must be equal to for the objects to be equal.
-        """
+        # print("name __eq__ called")
+
+        
+        # """All fields must be equal to for the objects to be equal.
+        # """
+
+
         return (type(self) == type(other) and self.name == other.name and
                 self.code == other.code and self.symbol == other.symbol and
                 self.digits == other.digits)
 
 
 class Money:
-    """
-    Represents an amount of money. Requires an amount and a currency.
-    """
+    # """
+    # Represents an amount of money. Requires an amount and a currency.
+    # """
 
     def __init__(self, amount, currency):
-        """
-        Parameters:
-        - amount -- quantity of currency
-        - currency -- type of currency
-        """
+
+        self.amount = amount
+        self.currency = currency
+
+        # """
+        # Parameters:
+        # - amount -- quantity of currency
+        # - currency -- type of currency
+        # """
         pass
 
-    def __str__(self):
-        """
-        Should use the currency symbol if available, else use the code.
-        Use the currency digits to determine number of digits to show.
-        """
-        pass
+def __str__(self):
+
+# """
+        # Should use the currency symbol if available, else use the code.
+        # Use the currency digits to determine number of digits to show.
+        # """
+        # pass
+
+    # self.money = []
+    if self.currency.symbol:
+        return f"{self.currency.symbol({self.amount}):.{self.currency.digits}f}"
+        else:
+            return f"{self.currency.code}{self.amount:.{self.currency.digits}f}" 
+                
 
     def __repr__(self):
-        return f"<Money {str(self)}>"
+        # repr formal representation --str__ informal representation
+            return f"<Money {str(self)}>"
 
     def __eq__(self, other):
+       
         """
         All fields must be equal to for the objects to be equal.
         """
